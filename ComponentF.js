2c36df083fd630c5c273273d036d1a1d82864fdb
santoshSrinivasa <santoshsrinivasa94@gmail.com>
2019-11-27 12:56:13 +0530

    1 file modified

--- ComponentF.js
+++ ComponentF.js
@@ -1,29 +1,41 @@
-import React from "react";
-import {UserContext,TitleContext} from "./index";
+import React, { useContext } from "react";
+import { UserContext, TitleContext } from "./index";
 
-class ComponentF extends React.Component{
-  render(){
-    return (
-     <UserContext.Consumer>
-     {
-       user => {
-         return (
-           <TitleContext.Consumer>
-           {
-             title => {
-               return (
-                 <div>
-                 {title} - {user}
-                 </div>
-               )
-             }
-           }
-           </TitleContext.Consumer>
-         )
-       }
-     }
-     </UserContext.Consumer>
-    )
-  }
+// class ComponentF extends React.Component {
+//   render() {
+//     return (
+//        <UserContext.Consumer>
+//        {
+//          user => {
+//            return (
+//              <TitleContext.Consumer>
+//              {
+//                title => {
+//                  return (
+//                    <div>
+//                    {title} - {user}
+//                    </div>
+//                  )
+//                }
+//              }
+//              </TitleContext.Consumer>
+//            )
+//          }
+//        }
+//        </UserContext.Consumer>
+//     );
+//   }
+// }
+
+function ComponentF() {
+  const user = useContext(UserContext);
+  const title = useContext(TitleContext);
+  console.log(user);
+  console.log(title);
+  return (
+    <div>
+      {title} - {user}
+    </div>
+  );
 }
 export default ComponentF;
